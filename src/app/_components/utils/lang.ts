bd7817dbae1e6eb1a361ae919942d7ea6aa67304
makino haruto <74090756+harutiro@users.noreply.github.com>
2024-01-23 17:07:55 +0900

    Revert "add: home のレイアウト"

--- src/app/_components/utils/lang.ts
+++
@@ -1,9 +0,0 @@
-import { TypeHome } from '../types/home';
-
-export function getLang(en: TypeHome, ja: TypeHome): TypeHome {
-  let lang = 'en';
-  if (typeof window !== 'undefined') {
-    lang = window.navigator.language;
-  }
-  return lang === 'en' ? en : ja;
-}
