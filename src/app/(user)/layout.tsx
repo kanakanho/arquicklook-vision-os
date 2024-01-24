f55170a8bb33461120a362de965d92c77c652fa9
kanakanho <kahoshibakichi@gmail.com>
2024-01-24 16:48:43 +0900

    fix: layoutのstyled-componentを削除

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,31 +1,27 @@
-'use client';
-
 import { ReactNode } from 'react';
-import styled from 'styled-components';
 
 interface Props {
   children: ReactNode;
 }
 
-const Header = styled.div`
-  text-align: center;
-  background-color: #000;
-  color: #eee;
-  font-weight: 600;
-  font-size: 32px;
-  padding: 10px 0;
-
-  position: fixed;
-  width: 100%;
-`;
-
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang='en'>
-      <body>
-        <Header>ARQuickLook for VisionOS</Header>
-        {children}
-      </body>
-    </html>
+    <body>
+      <header
+        style={{
+          textAlign: 'center',
+          backgroundColor: '#000',
+          color: '#eee',
+          fontWeight: 600,
+          fontSize: '32px',
+          padding: '10px 0',
+          position: 'fixed',
+          width: '100%',
+        }}
+      >
+        ARQuickLook for VisionOS
+      </header>
+      {children}
+    </body>
   );
 }
