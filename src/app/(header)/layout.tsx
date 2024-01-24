90f50ea1e984864b53467779bdcbb0055037e6c8
kanakanho <kahoshibakichi@gmail.com>
2024-01-24 17:04:14 +0900

    fix: layoutからstyled-componentを削除,Recoilの状態管理を切り出し

--- src/app/(header)/layout.tsx
+++ src/app/(header)/layout.tsx
@@ -1,30 +1,26 @@
-'use client';
-
 import { ReactNode } from 'react';
-import { RecoilRoot } from 'recoil';
-import styled from 'styled-components';
 
 interface Props {
   children: ReactNode;
 }
 
-const Header = styled.div`
-  text-align: center;
-  background-color: #000;
-  color: #eee;
-  font-weight: 600;
-  font-size: 32px;
-  padding: 10px 0;
-`;
-
 export default function RootLayout({ children }: Props) {
   return (
     <html lang='en'>
       <body>
-        <RecoilRoot>
-          <Header>ARQuickLook for VisionOS</Header>
-          {children}
-        </RecoilRoot>
+        <header
+          style={{
+            textAlign: 'center',
+            backgroundColor: '#000',
+            color: '#eee',
+            fontWeight: 600,
+            fontSize: '32px',
+            padding: '10px 0',
+          }}
+        >
+          ARQuickLook for VisionOS
+        </header>
+        {children}
       </body>
     </html>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 import './globals.css';
+import RecoilProvider from './RecoilProvider';
 
 type Props = {
   children: ReactNode;
@@ -14,7 +15,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <RecoilProvider>
+        <body>{children}</body>
+      </RecoilProvider>
     </html>
   );
 }
