29db82af1353499fd353a414e80480a07ac3a0b2
kanakanho <kahoshibakichi@gmail.com>
2024-01-24 17:05:13 +0900

    fix: フォーマット

--- src/app/RecoilProvider.tsx
+++ src/app/RecoilProvider.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { ReactNode } from "react";
-import { RecoilRoot } from "recoil";
+import { ReactNode } from 'react';
+import { RecoilRoot } from 'recoil';
 
 export default function RecoilProvider({ children }: { children: ReactNode }) {
   return <RecoilRoot>{children}</RecoilRoot>;
