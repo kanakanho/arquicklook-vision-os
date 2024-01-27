4883760cc5b74438efa41a94c8b60e1d06c70aa0
kanakanho <kahoshibakichi@gmail.com>
2024-01-28 05:11:27 +0900

    Merge branch 'main' into add-upload

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,9 +1,44 @@
+import { Metadata } from 'next';
 import { ReactNode } from 'react';
 import Header from './_components/Header';
 
 interface Props {
   children: ReactNode;
 }
+
+const siteName = 'AR Quick Look for VisionPro';
+const description = 'This site is a demo of AR Quick Look for VisionPro';
+const url = 'https://arquicklook-vision-os.vercel.app/';
+
+export const metadata: Metadata = {
+  title: siteName,
+  description: description,
+  applicationName: siteName,
+  authors: [
+    {
+      name: 'kanakanho',
+      url: 'https://github.com/kanakanho',
+    },
+    {
+      name: 'harutiro',
+      url: 'https://github.com/harutiro',
+    },
+  ],
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary',
+    title: siteName,
+    description,
+    site: '@Shiba_ao_',
+  },
+};
 
 export default function RootLayout({ children }: Props) {
   return (
