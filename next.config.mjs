a904fa51f3c04fbf704170a678717339a86bf531
kanakanho <kahoshibakichi@gmail.com>
2024-02-09 00:57:56 +0900

    add: safari用の設定

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  rewrites: [
+    {
+      source: "/__/auth/:path*",
+      destination: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}/__/auth/:path*`,  
+    },
+  ],
 };
 
 export default nextConfig;
